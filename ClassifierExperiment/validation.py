8f0f72909dc8a38f24e970d2bca7b54765d6faa9
dieckmdo <dominikdieckmann@gmx.de>
2018-12-11 17:27:28 +0100

    änderungen der finalen version, die bisher noch fehlten

--- ClassifierExperiment/validation.py
+++ ClassifierExperiment/validation.py
@@ -3,6 +3,8 @@
 
 import pickle
 import os
+import sys
+import shutil
 import numpy as np
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -24,104 +26,120 @@ def toLatexTab(accList, precList, recList, f1List, r):
         resultString += ' \\\  \n'
     return resultString
 
-
-dbFileName = 'RFUnrealClassifier.txt'
-
-predictionList = []
-predIdx = 0
-groundTruthList = []
-gtIdx = 0
-
-delimiter = '---'
-fs = open(dbFileName, 'r')
-dbs = fs.read().split(delimiter)
-
-for db in dbs:
-    lines = db.split('\n')
-    for l in lines:
-        if l.find('object') > -1:
-            groundTruthList.append(l)
-        elif l.find('instance') > -1:
-            predictionList.append(l)
-
-#############################################################
-## change list to only contain the objectnames
-#############################################################
-## create results Directory
-dirName = 'RFresults'
-os.mkdir(dirName)
-
-for x, entry in enumerate(groundTruthList):
-  groundTruthList[x] = entry.split(',')[1].split(')')[0]
-
-for x, entry in enumerate(predictionList):
-  predictionList[x] = entry.split(',')[1].split(')')[0]
-
-##########################################################
-## metrics
-##########################################################
-fileName = dirName + '/metrics.txt'
-
-fs = open(fileName, 'w')
-
-#print 'accuracy: ' + str(accuracy_score(groundTruthList, predictionList))
-fs.write('accuracy: ' + str(accuracy_score(groundTruthList, predictionList)) + '\n')
-
-# #print 'precision(macro): ' + str(precision_score(groundTruthList, predictionList, average='macro'))
-# #print 'precision(micro): ' + str(precision_score(groundTruthList, predictionList, average='micro'))
-# #print 'precision(None): ' + str(precision_score(groundTruthList, predictionList, average=None))
-fs.write('precision(macro): ' + str(round(precision_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
-fs.write('precision(micro): ' + str(round(precision_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
-fs.write('precision(None): ' + str(precision_score(groundTruthList, predictionList, average=None)) + '\n')
-
-# #print 'recall(macro): ' + str(recall_score(groundTruthList, predictionList, average='macro'))
-# #print 'recall(micro): ' + str(recall_score(groundTruthList, predictionList, average='micro'))
-# #print 'recall(None): ' + str(recall_score(groundTruthList, predictionList, average=None))
-fs.write('recall(macro): ' + str(round(recall_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
-fs.write('recall(micro): ' + str(round(recall_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
-fs.write('recall(None): ' + str(recall_score(groundTruthList, predictionList, average=None)) + '\n')
-
-# #print 'f1(macro): ' + str(f1_score(groundTruthList, predictionList, average='macro'))
-# #print 'f1(micro): ' + str(f1_score(groundTruthList, predictionList, average='micro'))
-# #print 'f1(None): ' + str(f1_score(groundTruthList, predictionList, average=None))
-fs.write('f1(macro): ' + str(round(f1_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
-fs.write('f1(micro): ' + str(round(f1_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
-fs.write('f1(None): ' + str(f1_score(groundTruthList, predictionList, average=None)) + '\n')
-
-# #print confusion_matrix(groundTruthList, predictionList)
-cm = confusion_matrix(groundTruthList, predictionList)
-
-allCases = len(groundTruthList)
-allCorrect = np.trace(cm)
-print allCorrect
-
-accuracys = []
-for i in range(0, len(cm)):
-    li = cm[i]
-    tp = li[i]
-    tn = allCorrect - tp
-    rowAll = 0
-    for k in range(0, len(li)):
-        rowAll += li[k]
     
-    fn = rowAll - tp
-    fp = 0
-    for k in range(0, len(cm)):
-        fp += cm[k][i]
-    fp -= tp
-    result = round((tp + tn) / float(tp + tn + fn + fp), 4)
-    accuracys.append(result)
+def computeResults(classifier):
+    
+    dbFileName = classifier + 'UnrealClassifier.txt'
 
-#print accuracys
+    predictionList = []
+    predIdx = 0
+    groundTruthList = []
+    gtIdx = 0
 
-fs.write('class accuracys: ' + str(accuracys).strip('[]'))
-fs.write('\n')
-fs.write('\n')
-fs.write(toLatexTab(accuracys, precision_score(groundTruthList, predictionList, average=None), recall_score(groundTruthList, predictionList, average=None), f1_score(groundTruthList, predictionList, average=None), 4 ))
-fs.write('\n')
-fs.write(toLatexTab(accuracys, precision_score(groundTruthList, predictionList, average=None), recall_score(groundTruthList, predictionList, average=None), f1_score(groundTruthList, predictionList, average=None), 2 ))
-plot_confusion_matrix(groundTruthList, predictionList, x_tick_rotation=90, figsize=(14,13), title=' ', text_fontsize='large', cmap='Reds')
-matrixFileName = dirName + '/confusionMatrix.png'
-plt.savefig(matrixFileName)
-#plt.show()
-fs.close()
+    delimiter = '---'
+    fs = open(dbFileName, 'r')
+    dbs = fs.read().split(delimiter)
+
+    for db in dbs:
+        lines = db.split('\n')
+        for l in lines:
+            if l.find('object') > -1:
+                groundTruthList.append(l)
+            elif l.find('instance') > -1:
+                predictionList.append(l)
+
+    #############################################################
+    ## change list to only contain the objectnames
+    #############################################################
+    ## create results Directory
+    dirName = classifier + 'results'
+    try:
+        os.mkdir(dirName)
+    except OSError:
+        shutil.rmtree(dirName)
+        os.mkdir(dirName)
+
+    for x, entry in enumerate(groundTruthList):
+      groundTruthList[x] = entry.split(',')[1].split(')')[0]
+
+    for x, entry in enumerate(predictionList):
+      predictionList[x] = entry.split(',')[1].split(')')[0]
+
+    ##########################################################
+    ## metrics
+    ##########################################################
+    fileName = dirName + '/metrics.txt'
+
+    fs = open(fileName, 'w')
+
+    #print 'accuracy: ' + str(accuracy_score(groundTruthList, predictionList))
+    fs.write('accuracy: ' + str(accuracy_score(groundTruthList, predictionList)) + '\n')
+
+    # #print 'precision(macro): ' + str(precision_score(groundTruthList, predictionList, average='macro'))
+    # #print 'precision(micro): ' + str(precision_score(groundTruthList, predictionList, average='micro'))
+    # #print 'precision(None): ' + str(precision_score(groundTruthList, predictionList, average=None))
+    fs.write('precision(macro): ' + str(round(precision_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
+    fs.write('precision(micro): ' + str(round(precision_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
+    fs.write('precision(None): ' + str(precision_score(groundTruthList, predictionList, average=None)) + '\n')
+
+    # #print 'recall(macro): ' + str(recall_score(groundTruthList, predictionList, average='macro'))
+    # #print 'recall(micro): ' + str(recall_score(groundTruthList, predictionList, average='micro'))
+    # #print 'recall(None): ' + str(recall_score(groundTruthList, predictionList, average=None))
+    fs.write('recall(macro): ' + str(round(recall_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
+    fs.write('recall(micro): ' + str(round(recall_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
+    fs.write('recall(None): ' + str(recall_score(groundTruthList, predictionList, average=None)) + '\n')
+
+    # #print 'f1(macro): ' + str(f1_score(groundTruthList, predictionList, average='macro'))
+    # #print 'f1(micro): ' + str(f1_score(groundTruthList, predictionList, average='micro'))
+    # #print 'f1(None): ' + str(f1_score(groundTruthList, predictionList, average=None))
+    fs.write('f1(macro): ' + str(round(f1_score(groundTruthList, predictionList, average='macro'), 4)) + '\n')
+    fs.write('f1(micro): ' + str(round(f1_score(groundTruthList, predictionList, average='micro'), 4)) + '\n')
+    fs.write('f1(None): ' + str(f1_score(groundTruthList, predictionList, average=None)) + '\n')
+
+    # #print confusion_matrix(groundTruthList, predictionList)
+    cm = confusion_matrix(groundTruthList, predictionList)
+
+    allCases = len(groundTruthList)
+    allCorrect = np.trace(cm)
+    print allCorrect
+
+    accuracys = []
+    for i in range(0, len(cm)):
+        li = cm[i]
+        tp = li[i]
+        tn = allCorrect - tp
+        rowAll = 0
+        for k in range(0, len(li)):
+            rowAll += li[k]
+        
+        fn = rowAll - tp
+        fp = 0
+        for k in range(0, len(cm)):
+            fp += cm[k][i]
+        fp -= tp
+        result = round((tp + tn) / float(tp + tn + fn + fp), 4)
+        accuracys.append(result)
+
+    #print accuracys
+
+    fs.write('class accuracys: ' + str(accuracys).strip('[]'))
+    fs.write('\n')
+    fs.write('\n')
+    fs.write(toLatexTab(accuracys, precision_score(groundTruthList, predictionList, average=None), recall_score(groundTruthList, predictionList, average=None), f1_score(groundTruthList, predictionList, average=None), 4 ))
+    fs.write('\n')
+    fs.write(toLatexTab(accuracys, precision_score(groundTruthList, predictionList, average=None), recall_score(groundTruthList, predictionList, average=None), f1_score(groundTruthList, predictionList, average=None), 2 ))
+    plot_confusion_matrix(groundTruthList, predictionList, x_tick_rotation=90, figsize=(14,13), title=' ', text_fontsize='large', cmap='Reds')
+    matrixFileName = dirName + '/confusionMatrix.png'
+    plt.savefig(matrixFileName)
+    #plt.show()
+    fs.close()
+
+def main():
+    if (len(sys.argv) != 2):
+        print 'Wrong paramter count! Needs a parameter for the classifier name.'
+    else:
+        computeResults(sys.argv[1])
+
+if __name__=="__main__":
+    main()
+    
